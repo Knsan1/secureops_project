ab44e1b32d2b5fc94d8cfe16e329efadc3cfec1c
kns <kns@gmail.com>
2025-01-06 13:42:19 +0000

    Modify variable for 09

--- 09-tfc-create-rds-ec2-instance/tfc-workspace-variables/variable.tf
+++ 09-tfc-create-rds-ec2-instance/tfc-workspace-variables/variable.tf
@@ -8,22 +8,22 @@ variable "org_name" {
   type        = string
   default     = "SecureOps_Project" ### have to change ORG ID
 }
-variable "vault_url" {
-  description = "The address of the Vault instance runs will access."
-  type        = string
-  default     = "https://hcp-vault-cluster-public-vault-bde3da3c.a82db95d.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
-}
-variable "run_role" {
-  description = "TFC_VAULT_RUN_ROLE"
-  type        = string
-  default     = "tfc-admin-role"
-}
-variable "vault_namespace" {
-  description = "TFC_VAULT_NAMESPACE"
-  type        = string
-  default     = "admin"
-}
 
+# variable "vault_url" {
+#   description = "The address of the Vault instance runs will access."
+#   type        = string
+#   default     = "https://hcp-vault-cluster-public-vault-bde3da3c.a82db95d.z1.hashicorp.cloud:8200" ### have to change new vault cluster id
+# }
+# variable "run_role" {
+#   description = "TFC_VAULT_RUN_ROLE"
+#   type        = string
+#   default     = "tfc-admin-role"
+# }
+# variable "vault_namespace" {
+#   description = "TFC_VAULT_NAMESPACE"
+#   type        = string
+#   default     = "admin"
+# }
 
 variable "aws_region" {
   description = "AWS_REGION"
